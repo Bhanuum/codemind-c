e6a648a9b3d46113e271344f731881cf3f2dd5ef
Bhanuum <101499789+Bhanuum@users.noreply.github.com>
2022-06-14 21:59:10 +0530

    #include<stdio.h> int main() {     int n,i,c,v;     scanf("%d",&n);     int a[n];     c=0;     v=0;     for(i=1;i<=n;i++)     {         scanf("%d",&a[i]);     }     for(i=1;i<=n;i++)     {         if (i%2!=0)         {             c+=a[i];         }     }     printf("%d",c); }

---
+++ Sum_of_Even_Positions.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,c,v;
+    scanf("%d",&n);
+    int a[n];
+    c=0;
+    v=0;
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=1;i<=n;i++)
+    {
+        if (i%2!=0)
+        {
+            c+=a[i];
+        }
+    }
+    printf("%d",c);
+}
