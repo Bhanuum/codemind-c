2c2f983f6b573ec8751c209d0ed582e363864e97
Bhanuum <101499789+Bhanuum@users.noreply.github.com>
2022-06-14 22:37:49 +0530

    #include<stdio.h> int main() {     int n,i,c,v,s;     scanf("%d",&n);     int a[n];     c=0;     v=0;     for(i=1;i<=n;i++)     {         scanf("%d",&a[i]);         c=c+a[i];     }     printf("%d",c); }

---
+++ Sum_of_Array.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,c,v,s;
+    scanf("%d",&n);
+    int a[n];
+    c=0;
+    v=0;
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&a[i]);
+        c=c+a[i];
+    }
+    printf("%d",c);
+}    
