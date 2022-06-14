7ea0a9949c6de362d90672bb184997d119bd1358
Bhanuum <101499789+Bhanuum@users.noreply.github.com>
2022-06-14 22:06:55 +0530

    #include<stdio.h> int main() {     int n,i,c,v;     scanf("%d",&n);     int a[n];     c=0;     v=0;     for(i=1;i<=n;i++)     {         scanf("%d",&a[i]);         if (a[i]%2!=0)         {             c+=a[i];         }     }     printf("%d",c); }

---
+++ ODD_Sum.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,c,v;
+    scanf("%d",&n);
+    int a[n];
+    c=0;
+    v=0;
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&a[i]);
+        if (a[i]%2!=0)
+        {
+            c+=a[i];
+        }
+    }
+    printf("%d",c);
+}
