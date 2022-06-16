a1c0dfd9232390a9b1f7c362d0b28271d6424ecc
Bhanuum <101499789+Bhanuum@users.noreply.github.com>
2022-06-16 22:36:14 +0530

    #include<stdio.h> int main() {     int n,k,i;     scanf("%d",&n);     int a[i];     for(i=1;i<=n;i++){     scanf("%d",&a[i]);     }     k=0;     for(i=1;i<=n;i++)     {         if (a[i]%2==0)         {             k+=a[i];         }         else         {             break;         }     }     printf("%d",k); }

---
+++ sum_of_array__elements_upto__first_odd_number.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,k,i;
+    scanf("%d",&n);
+    int a[i];
+    for(i=1;i<=n;i++){
+    scanf("%d",&a[i]);
+    }
+    k=0;
+    for(i=1;i<=n;i++)
+    {
+        if (a[i]%2==0)
+        {
+            k+=a[i];
+        }
+        else 
+        {
+            break;
+        }
+    }
+    printf("%d",k);
+}
